61835b5162c80cbd8e681dcf80f0a4d553d7cc62
StefanUp1 <stefan_ja95@hotmail.com>
2018-10-20 00:18:52 +0200

    First commit

--- src/components/Today.vue
+++ src/components/Today.vue
@@ -2,29 +2,29 @@
   <div>
     <h1>{{ todayDate }}</h1>
 
+<!--
     <h1 v-if="noFixtures">
       No games today.
     </h1>
+-->
 
-    <div v-else>
-      <ul class="list-group">
-        <li v-if="fixture.match_live == 1" class="list-group-item" v-for="fixture in fixtures">
-          <router-link :to="{name: 'singleFixture', params: {fixture: fixture}}">
-            <span class="badge badge-secondary info">
-              LIVE
-            </span>
-            {{ fixture.match_hometeam_name }} - {{ fixture.match_awayteam_name}}
-          </router-link>
-        </li>
-
-        <li v-else class="list-group-item">
-          <span class="info">
-            {{ fixture.match_time }}h
+    <ul class="list-group">
+      <li v-if="fixture.match_live == 1" class="list-group-item" v-for="fixture in fixtures">
+        <router-link :to="{name: 'singleFixture', params: {fixture: fixture}}">
+          <span class="badge badge-secondary info">
+            LIVE
           </span>
           {{ fixture.match_hometeam_name }} - {{ fixture.match_awayteam_name}}
-        </li>
-      </ul>
-    </div>
+        </router-link>
+      </li>
+
+      <li v-else class="list-group-item">
+        <span class="info">
+          {{ fixture.match_time }}h
+        </span>
+        {{ fixture.match_hometeam_name }} - {{ fixture.match_awayteam_name}}
+      </li>
+    </ul>
 
   </div>
 </template>
@@ -48,7 +48,7 @@
     },
     methods: {
       getFixtures() {
-        fetch(`https://apifootball.com/api/?action=get_events&from=todayDate&to=todayDate&league_id=63&APIkey=51d9ac8fbe11e79e2bcba99d48a61323f5ebf0a30b582607f9318f9d97236b2c`)
+        fetch(`https://apifootball.com/api/?action=get_events&from=${this.todayDate}&to=${this.todayDate}&league_id=63&APIkey=51d9ac8fbe11e79e2bcba99d48a61323f5ebf0a30b582607f9318f9d97236b2c`)
           .then(res => res.json())
           .then(data => {
             if (data.error) {
